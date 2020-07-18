2779cb89d52b3faa78ffbb653e831bc819af14d9
Dev-Mediane <03082190015@student.uph.edu>
2020-07-18 15:58:55 +0700

    Ada perbaikan, tapi nanti jam 5 kayaknya bisa siap

--- Final_Project_PBO/src/ManajemenKaryawan/PenghasilanKaryawan.java
+++ Final_Project_PBO/src/ManajemenKaryawan/PenghasilanKaryawan.java
@@ -8,37 +8,34 @@ import java.util.Date;
 
 public class PenghasilanKaryawan
 {
-    private int totalPenghasilan;
-    private Date jamMulaiKerja;
-    private Date jamSelesaiKerja;
-    private String statusKerja;
-    private int jumlahHariKerja;
+    AkunKaryawan dataAkunKaryawan;
+    private Integer totalPenghasilan;
     private double lamaWaktuKerja;
     private int selisihJam, selisihMenit;
 
-    int penghasilanPerJam(String statusKerja)
+    int penghasilanPerJam()
     {
         int upahPerJam;  // jumlah upah/ gaji yang dihitung per jamnya
 
-        if (statusKerja.equals("full time"))
+        if (dataAkunKaryawan.getDataJadwalKerjaKaryawan().getStatusKerja().equals("free time"))
         {
             // FULL TIME
             upahPerJam = 5000000 / 173;
             return upahPerJam;
         }
-        else if (statusKerja.equals("part time"))
+        else if (dataAkunKaryawan.getDataJadwalKerjaKaryawan().getStatusKerja().equals("part time"))
         {
             // PART TIME
             upahPerJam = 2000000 / 173;
             return upahPerJam;
         }
-        else if (statusKerja.equals("internship"))
+        else if (dataAkunKaryawan.getDataJadwalKerjaKaryawan().getStatusKerja().equals("internship"))
         {
             // INTERNSHIP
             upahPerJam = 3000000 / 173;
             return upahPerJam;
         }
-        else if (statusKerja.equals("freelance"))
+        else if (dataAkunKaryawan.getDataJadwalKerjaKaryawan().getStatusKerja().equals("freelance"))
         {
             // FREELANCE
             upahPerJam = 4000000 / 173;
@@ -47,29 +44,33 @@ public class PenghasilanKaryawan
         else {return 0;}
     }
 
-    int calculate(AkunKaryawan dataAkunKaryawan) throws ParseException
+    Integer calculate()
     {
+        System.out.println("Status Kerja = " + dataAkunKaryawan.getDataJadwalKerjaKaryawan().getStatusKerja());
+        Date jamMulaiKerja, jamSelesaiKerja;
+        String statusKerja; Integer jumlahHariKerja;
+
         // Isi = data jadwal kerja yang diproses oleh karyawan
-        this.jamMulaiKerja = dataAkunKaryawan.getDataJadwalKerjaKaryawan().getJamMulaiKerja();
-        this.jamSelesaiKerja = dataAkunKaryawan.getDataJadwalKerjaKaryawan().getJamSelesaiKerja();
-        this.statusKerja = dataAkunKaryawan.getDataJadwalKerjaKaryawan().getStatusKerja();
-        this.jumlahHariKerja = dataAkunKaryawan.getDataJadwalKerjaKaryawan().getJumlahHariKerja();
+        jamMulaiKerja = dataAkunKaryawan.getDataJadwalKerjaKaryawan().getJamMulaiKerja();
+        jamSelesaiKerja = dataAkunKaryawan.getDataJadwalKerjaKaryawan().getJamSelesaiKerja();
+        statusKerja = dataAkunKaryawan.getDataJadwalKerjaKaryawan().getStatusKerja();
+        jumlahHariKerja = dataAkunKaryawan.getDataJadwalKerjaKaryawan().getJumlahHariKerja();
 
         // perbedaan jam mulai kerja dan jam selesai kerja
-        double timeDifference01 = this.jamSelesaiKerja.getTime() - this.jamMulaiKerja.getTime();
+        double timeDifference01 = jamSelesaiKerja.getTime() - jamMulaiKerja.getTime();
 
         selisihJam = (int) Math.floor(timeDifference01 / 3600000); double timeDifference02 = timeDifference01 % 3600000;
         selisihMenit = (int) Math.floor(timeDifference02 / 60000);
 
-        this.lamaWaktuKerja = selisihJam + (selisihMenit / 60);
+        lamaWaktuKerja = selisihJam + (selisihMenit / 60);
 
-        this.totalPenghasilan = (int) (this.lamaWaktuKerja * this.jumlahHariKerja * penghasilanPerJam(statusKerja));
+        totalPenghasilan = (int) (lamaWaktuKerja * jumlahHariKerja * penghasilanPerJam());
         return this.totalPenghasilan;
     }
 
-    void display(AkunKaryawan dataAkunKaryawan)
+    void display()
     {
         // Isi = tampilkan Slip Gaji yang akan diterima pegawai/ karyawan
-        System.out.println("Total diterima   = " + calculate(dataAkunKaryawan));
+        System.out.println("Total diterima   = " + calculate());
     }
 }
